2b5b49647671a8b3669ac2c0ce38cdfdb1f17c30
AdityaKalambe <adityakalambe20@gmail.com>
2023-09-05 19:21:27 +0530

    Basis JDBC
    Signed-off-by: AdityaKalambe <adityakalambe20@gmail.com>

---
+++ src/BasisJDBC_01/Practice01.java
@@ -0,0 +1,20 @@
+package BasisJDBC_01;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+class Practice01{
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from students");
+        ArrayList<Student> studentsArrayList = new ArrayList<>();
+        while (resultSet.next()){
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("StudentName");
+            studentsArrayList.add(new Student(id,name));
+        }
+        System.out.println(studentsArrayList);
+
+    }
+}
