17b4379b17c8d261be13ee01c4c533e94b6b7e0a
AdityaKalambe01 <adityakalambe20@gmail.com>
2023-09-07 17:48:29 +0530

    Credentials
    Signed-off-by: AdityaKalambe01 <adityakalambe20@gmail.com>

---
+++ src/Credentials/MySQL.java
@@ -0,0 +1,7 @@
+package Credentials;
+
+public class MySQL {
+    public static final String url="jdbc:mysql://localhost:3306/";
+    public static final String username = "root";
+    public static final String password = "root";
+}
